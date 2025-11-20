554f5d37683fee7f093c7516dcde67394615188e
jlinreck <jlinreck@terpmail.umd.edu>
2025-11-20 18:55:05 -0500

    pushed

--- forge.py
+++ forge.py
@@ -98,20 +98,46 @@ def universal_forge(MAC, msg):
     if m == 15 and n == 15:
         return None
     if m < 15 and n < 15:
-        s1, t1 = MAC((m + 1, n))
-        s2, t2 = MAC((m, n + 1))
-        s3, t3 = MAC((m + 1, n + 1))
-        return (s1 + s2 - s3, t1 + t2 - t3)
+        if m > 0 and n > 0:
+            s4, t4 = MAC((m - 1, n - 1))
+            s5, t5 = MAC((m, n - 1))  
+            s6, t6 = MAC((m - 1, n))
+            return (s6 + t5 - t4, t6 + s5 - s4)
+        else:
+            if m == 0 and n == 0:
+                s1, t1 = MAC((0, 1))
+                s2, t2 = MAC((1, 0))
+                return (t1 - s1, t1 - s1)
+            elif m == 0:
+                s1, t1 = MAC((0, n - 1))
+                s3, t3 = MAC((0, n + 1))
+                return (t3 - s3 - t1, t3 - s3)
+            else:  # n == 0
+                s1, t1 = MAC((m - 1, 0))
+                s3, t3 = MAC((m + 1, 0))
+                return (s3 - t3, s3 - t3 - s1)
     elif m == 15:
-        s1, t1 = MAC((14, n))
-        s2, t2 = MAC((14, n + 1))
-        s3, t3 = MAC((15, n + 1))
-        return (s3 + s1 - s2, t3 + t1 - t2)
-    else: 
-        s1, t1 = MAC((m, 14))
-        s2, t2 = MAC((m + 1, 14))
-        s3, t3 = MAC((m + 1, 15))
-        return (s3 + s1 - s2, t3 + t1 - t2)
+        if n > 0:
+            s4, t4 = MAC((14, n - 1))
+            s5, t5 = MAC((15, n - 1))
+            s6, t6 = MAC((14, n))
+            return (s6 + t5 - t4, t6 + s5 - s4)
+        else:  # n == 0, m == 15
+            s1, t1 = MAC((14, 0))
+            s2, t2 = MAC((15, 1))
+            return (2*s1 - s2 + t2, t2 + s1 - s2)
+    
+    else:  # n == 15
+        if m > 0:
+            s4, t4 = MAC((m - 1, 14))
+            s5, t5 = MAC((m, 14))
+            s6, t6 = MAC((m - 1, 15))
+            return (s6 + t5 - t4, t6 + s5 - s4)
+        else:  # m == 0, n == 15
+            s1, t1 = MAC((0, 14))
+            s2, t2 = MAC((1, 15))
+            s3, t3 = MAC((1, 14))
+            return (s1 + t2 - t3, t1 + s2 - s3)
 
 def md_forge(MD):
     # In this task, you must return a msg of your choice with a valid tag that would be produced by running the Hash-and-MAC protocol specified by sample_MD
